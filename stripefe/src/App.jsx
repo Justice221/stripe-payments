b0017de9ecee873f0d3eef320148b790aac8e7af
Jay <joypatel1337@proton.me>
2023-12-12 18:24:13 +0530

    End to end stripe payments

---
+++ stripefe/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Cancel from "./components/Cancel";
+import Success from "./components/Success";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/cancel" element={<Cancel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
